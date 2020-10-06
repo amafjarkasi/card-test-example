90b60e3d989e45d4525d31940cb9785e90d486b1
amafjarkasi <amafjarkasi@gmail.com>
2020-10-06 15:24:46 +0000

    card example update

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-let who = ['The dog','My grandma','His turtle','My bird'];
-let action = ['ate','peed','crushed','broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+function randomSuitInfo() {
 
-function randomExcuses(parameterVariable) {
-    let randomWhoNumber = Math.floor(Math.random() * parameterVariable.length);
-    return randomWhoNumber;
+    let cardSuit = ["&#9824;", "<font color=red>&#9829;</font>", "&#9827;", "&#9830;"];
+
+    let suitCalc = cardSuit[Math.floor(Math.random() * cardSuit.length)];
+
+    return suitCalc;
+
 }
 
-function buildExcuses() {
-    let sentenceExcuses = "";
-    sentenceExcuses = `${who[randomExcuses(who)]} ${action[randomExcuses(action)]} ${what[randomExcuses(what)]} ${when[randomExcuses(when)]}`;
+function randomNumberInfo() {
 
-    document.getElementById("excuse"). innerHTML = sentenceExcuses;
+    let cardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+    let cardCalc = cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+
+    return cardCalc;
+
+}
+
+function buildCardInfo() {
+
+    //let sentenceExcuses = "";
+
+    //sentenceExcuses = `${who[randomExcuses(who)]} ${action[randomExcuses(action)]} ${what[randomExcuses(what)]} ${when[randomExcuses(when)]}`;
+    let suitInfo = randomSuitInfo();
+
+    document.getElementById("suitTop"). innerHTML = suitInfo;
+    document.getElementById("suitBottom"). innerHTML = suitInfo;
+
+    document.getElementById("numbersCard"). innerHTML = randomNumberInfo();    
+
 }
 
